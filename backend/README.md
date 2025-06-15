e5af1e50d914bfa98d0f1db0948d62cfa2d5bb48
Mrinmoy Mondal <mrinmoymondalreal+github@gmail.com>
2025-06-15 16:06:53 +0530

    added documentation

--- backend/README.md
+++ backend/README.md
@@ -22,6 +22,8 @@ npm start
 
 Navigate to `http://localhost:3000`
 
+for auth i am using better-auth so refer to better-auth docs for frontend
+
 - Use this inside lib/auth-client.ts in react/nextjs app
 
 ```
@@ -29,6 +31,30 @@ import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     baseURL: "http://localhost:3000"
 })
+```
+
+i have modified the some functions of better-auth like this
+
+```
+authClient.signUp.email({
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        image, // User image URL (optional)
+        callbackURL: "/dashboard", // A URL to redirect to after the user verifies their email (optional)
+        userType: "client" | "advocate" // Required
+    } as any); // use this any to aviod ts error
+```
+
+```
+authClient.updateUser({
+        name?, // user display name
+        image?, // User image URL (optional)
+        district?,
+        city?,
+        location?,
+        state?
+    } as any); // use this any to aviod ts error
 ```
 
 - to use this auth client refer to this page [AuthClient Documentation](https://www.better-auth.com/docs/basic-usage)
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,16 @@
 import express from "express";
 import logger from "morgan";
-import * as path from "path";
-
-import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
+import cookieParser from "cookie-parser";
 
 import { index } from "./routes/index";
 import authRouter from "./routes/auth";
 import cors from "cors";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 
 export const app = express();
+
+app.use(cookieParser());
 
 app.use(
   cors({
@@ -17,15 +19,36 @@ app.use(
   }),
 );
 
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LegalLink API",
+      version: "1.0.0",
+    },
+    components: {
+      securitySchemes: {
+        cookieAuth: {
+          type: "apiKey",
+          in: "cookie",
+          name: "better-auth.session_token",
+        },
+      },
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
 app.set("port", process.env.PORT || 3000);
 
 app.use(logger("dev"));
+
+app.use(authRouter);
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", index);
-app.use(authRouter);
-
-app.use(errorNotFoundHandler);
-app.use(errorHandler);
